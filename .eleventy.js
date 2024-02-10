a496664328516c838ae520ef7b34b543afc08c61
Justin Duke <justin@buttondown.email>
2024-02-10 14:33:04 -0500

    Add footnotes

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,23 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+let markdownIt = require("markdown-it");
+let markdownItFootnote = require("markdown-it-footnote");
+
+const STATIC_FILES = ["img", "scripts", "favicon.ico", "styles"];
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({
     "style.out.css": "style.css",
   });
 
-  eleventyConfig.addPassthroughCopy("img");
-  eleventyConfig.addPassthroughCopy("favicon.ico");
-
+  STATIC_FILES.forEach((file) => {
+    eleventyConfig.addPassthroughCopy(file);
+  });
   eleventyConfig.addPlugin(syntaxHighlight);
+
+  let options = {
+    html: true, // Enable HTML tags in source
+    linkify: true, // Autoconvert URL-like text to links
+  };
+  let markdownLib = markdownIt(options).use(markdownItFootnote);
+  eleventyConfig.setLibrary("md", markdownLib);
 };
---
+++ catalogs/great-essays.md
@@ -0,0 +1,10 @@
+---
+layout: post.njk
+title: Great essays
+tags: catalog
+date: "2024-02-10"
+---
+
+## Programming
+
+- [Parse, don't validate](https://lexi-lambda.github.io/blog/2019/11/05/parse-don-t-validate/)
